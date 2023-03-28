202c6c7f93862b74230c995e7d89f2b4af268e34
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-03-28 23:52:31 +0100

    Setup Deploy

--- src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private String theAllowedOrigins = "http://localhost:3000";
+    private String theAllowedOrigins = "https://finedefinition.github.io/library-project/";
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
