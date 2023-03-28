cc65b350ece364181c673fd56a4fd736c7288b9a
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-03-28 23:05:52 +0100

    Setup Railway

--- src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private String theAllowedOrigins = "http://localhost:3000";
+    private String theAllowedOrigins = "http://library-frontend.s3-website.eu-north-1.amazonaws.com";
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
